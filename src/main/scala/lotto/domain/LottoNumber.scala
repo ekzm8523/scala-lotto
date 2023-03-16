fc3745655bdd0b17d4cf6b95efe700514ea1c3a3
ekzm8523 <mjw8523@gmail.com>
2023-03-16 09:32:47 +0900

    refactor: CACHE_LOTTO_NUMBER 자료구조 Array -> Map으로 변경

--- src/main/scala/lotto/domain/LottoNumber.scala
+++ src/main/scala/lotto/domain/LottoNumber.scala
@@ -1,13 +1,12 @@
 package lotto.domain
 
-case class LottoNumber (number: Int) {}
 
 object LottoNumber{
     private val MIN_LOTTO_NUMBER = 1
     private val MAX_LOTTO_NUMBER = 45
-    private val CACHED_LOTTO_NUMBER: Array[LottoNumber] = (MIN_LOTTO_NUMBER - 1 to MAX_LOTTO_NUMBER)
-                                                                .map{num => new LottoNumber(num)}
-                                                                .toArray
+    private val CACHED_LOTTO_NUMBER: Map[Int, LottoNumber] = (LottoNumber.MIN_LOTTO_NUMBER to MAX_LOTTO_NUMBER)
+      .map { num => num -> new LottoNumber(num) }
+      .toMap
 
     def apply(number: Int): LottoNumber = {
         require(MIN_LOTTO_NUMBER <= number && number <= MAX_LOTTO_NUMBER)
@@ -15,3 +14,11 @@ object LottoNumber{
     }
 
 }
+
+class LottoNumber (val number: Int) {
+
+    if (number < LottoNumber.MIN_LOTTO_NUMBER || LottoNumber.MAX_LOTTO_NUMBER < number)
+        throw new IllegalArgumentException(s"숫자는 $LottoNumber.MIN_LOTTO_NUMBER 부터 $LottoNumber.MAX_LOTTO_NUMBER 사이의 값을 가져야 합니다.")
+    override def toString: String = number.toString
+}
+
